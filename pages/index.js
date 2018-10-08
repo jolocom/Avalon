b8858702cbf257205dad0ac574729893d90276dc
Flixow <danielsiwek95@gmail.com>
2018-10-08 11:41:43 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+
+class Home extends Component {
+  static async getInitialProps() {
+  }
+
+  render() {
+    return (
+      <>
+        Hello world
+      </>
+    );
+  }
+}
+
+export default Home;
