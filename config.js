8671631078b6eace077af90c1f7bb606e13725c8
Flixow <danielsiwek95@gmail.com>
2018-10-17 16:19:06 +0200

    Add environment variable for BASE_URL

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Private key associated with the service's identity
 const privateIdentityKey = Buffer.from('3d77b86e0bf3d822d477c6fd2071a5a8488a1a63c46088d9e93e61fa0e2575bf', 'hex');
 
-// Where is your service deployed. E.g. https://demo-sso.jolocom.com
-const serviceUrl = 'http://localhost:3000';
+// Where is your service deployed. E.g. https://jolocom.herokuapp.com/
+const serviceUrl = process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:3000';
 
 const credentialRequirements = {
   email: {
