9b2798a24f20ff7c44973fe457840c97f872095c
Flixow <danielsiwek95@gmail.com>
2018-10-18 16:39:26 +0200

    Log token for authentication endpoint

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ const configureRoutes = async(app, redisApi) => {
   app.post('/authentication/:identifier', async(req, res, next) => {
     const { identifier } = req.params;
     const { token } = req.body;
-
+    console.log({ token });
     try {
       const { credentialResponse } = await JSONWebToken.decode(token);
 
