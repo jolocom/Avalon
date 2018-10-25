28550a1fd5fbd26d738ad5d9930cccb53c9b6121
nataschaberg <natalja.wittenberg@gmx.de>
2018-10-25 17:02:04 +0200

    remove email cred req

--- server/sockets.js
+++ server/sockets.js
@@ -35,7 +35,6 @@ const configureSockets = (
       credentialRequest: {
         callbackURL,
         credentialRequirements: [
-          serverRuntimeConfig.credentialRequirements.email,
           serverRuntimeConfig.credentialRequirements.name,
         ],
       },
