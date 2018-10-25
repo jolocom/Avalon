e88e4065d03e8d2d55293e973840d828a61167e8
nataschaberg <natalja.wittenberg@gmx.de>
2018-10-25 17:04:09 +0200

    fix

--- server/routes.js
+++ server/routes.js
@@ -1,53 +1,18 @@
 const { JolocomLib } = require('jolocom-lib');
 const { JSONWebToken } = require('jolocom-lib/js/interactionFlows/JSONWebToken');
 const { CredentialRequest } = require('jolocom-lib/js/interactionFlows/credentialRequest/credentialRequest');
-const { claimsMetadata } = require('cred-types-jolocom-demo');
 
 const { validateCredentialSignatures, extractDataFromClaims } = require('./utils');
 
-const configureRoutes = async(app, redisApi, iw) => {
-  const { setAsync, delAsync, getAsync } = redisApi;
+const configureRoutes = async(app, redisApi) => {
+  const { setAsync } = redisApi;
   const getConfig = require('next/config').default;
   const { serverRuntimeConfig } = getConfig();
-
-  app.post('/receive/:type', async(req, res) => {
-    const { token } = req.body;
-    const { type } = req.params;
-    const { iss } = await JolocomLib.parse.interactionJSONWebToken.decode(token);
-    const did = iss.substring(0, iss.indexOf('#'));
-
-    const metadataMap = {
-      residency: claimsMetadata.demoId,
-      driving: claimsMetadata.demoDriversLicence,
-    };
-
-    const user = await getAsync(did);
-    await delAsync(did);
-    const parsed = JSON.parse(user);
-    const filtered = Object.keys(parsed).filter(key => key !== 'status');
-
-    const residencySignedCredential = await iw.create.signedCredential({
-      metadata: metadataMap[type],
-      claim: filtered.reduce((acc, curr) => ({ ...acc, [curr]: parsed[curr] }), {}),
-      subject: did,
-    });
-
-    const encodedCredential = await iw.create
-      .credentialsReceiveJSONWebToken({
-        iss: iw.getIdentity().getDID(),
-        typ: 'credentialsReceive',
-        credentialsReceive: {
-          signedCredentials: [residencySignedCredential.toJSON()]
-        },
-      })
-      .encode();
-
-    res.json({ token: encodedCredential });
-  });
 
   app.post('/authentication/:identifier', async(req, res, next) => {
     const { identifier } = req.params;
     const { token } = req.body;
+    console.log({ token });
     try {
       const { credentialResponse } = await JSONWebToken.decode(token);
 
@@ -61,7 +26,7 @@ const configureRoutes = async(app, redisApi, iw) => {
       });
 
       if (!credentialResponse.satisfiesRequest(credentialRequest)) {
-        throw new Error('The supplied credentials do not match the types of the requested credentials')
+        throw new Error('The supplied credentials do not match the types of the requested credentials');
       }
 
       const userData = {
