2f82f412daafdbaa140343a65ccf9303ea7faa3b
Flixow <danielsiwek95@gmail.com>
2018-11-05 14:09:46 +0100

    Add viewport meta

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link async rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
           <link rel="stylesheet" href="/static/fonts/fonts.css" />
 
