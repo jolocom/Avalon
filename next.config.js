cb4a5aec486827871c2c4432aa615327a618e60d
Kasia Szawan <kat.szwn@gmail.com>
2018-11-06 13:16:40 +0100

    Add and configure babel/polyfills with babel/preset_env to fix IE compatibility

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const webpack = require('webpack');
 
 module.exports = {
   webpack: config => {
+    // Unshift polyfills in main entrypoint.
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      if (entries['_app.js']) {
+        entries['_app.js'].unshift('./utils/polyfills.js');
+      }
+      return entries;
+    }
     config.plugins.push(
       new webpack.EnvironmentPlugin(localEnv)
     );
